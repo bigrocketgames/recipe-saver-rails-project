40a4ec39014915e28fd7b76930f312b2041e5644
Nathan Robbert <admin@bigrocketgames.com>
2017-02-08 21:10:23 -0800

    Added validations for presence in collection, ingredient, and recipe models.

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,5 +1,6 @@
 class Collection < ApplicationRecord
   belongs_to :cook, class_name: 'User'
   belongs_to :food, class_name: 'Recipe'
+  validates :name, presence: true
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   has_many :Ingredients
   has_many :collections
   has_many :cooks, through: :collections, class_name: 'User'
-  
+  validates :name, :user_id, presence: true
   accepts_nested_attributes_for :ingredients
 
 end
