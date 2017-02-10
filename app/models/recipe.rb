761fd58917e895077b29d4072f914b8308e8c8ef
Nathan Robbert <admin@bigrocketgames.com>
2017-02-10 10:53:42 -0800

    Changed page redirect to recipes_path after sign in.  Got recipes_path to show.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :Ingredients
+  has_many :ingredients
   has_many :collections
   has_many :cooks, through: :collections, class_name: 'User'
   validates :name, :user_id, presence: true
