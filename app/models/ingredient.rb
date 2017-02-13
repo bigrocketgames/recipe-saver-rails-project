a9b9ba5578179e0394603085dc05d47e00166211
Nathan Robbert <admin@bigrocketgames.com>
2017-02-12 20:16:32 -0800

    Added recipe and ingredient validations for name uniqueness.  Updated recipe form with note of steps needed.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,6 @@ class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
   validates :name, :quantity, presence: true
+  validates :name, uniqueness: true
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,7 @@ class Recipe < ApplicationRecord
   has_many :collections
   has_many :cooks, through: :collections, class_name: 'User'
   validates :name, :user_id, presence: true
+  validates :name, uniqueness: true
   accepts_nested_attributes_for :ingredients
 
 end
