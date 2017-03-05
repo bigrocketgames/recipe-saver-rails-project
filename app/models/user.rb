5fc176c2a0f2ba0da2ea3ff2a2275ba1659e2892
Nathan Robbert <admin@bigrocketgames.com>
2017-03-05 13:04:29 -0800

    Fixed devise oAuth through Github signin.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:github]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:github]
 
   has_many :recipes
   has_many :collections, foreign_key: :cook_id
