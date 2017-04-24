02eb4d6126866f690216ae61fe1f08ea6c18ffc7
Nathan <admin@bigrocketgames.com>
2017-04-23 19:58:04 -0700

    Updated user/show.html.erb to have a link to clear the list of recipes from the user's page. Updated users.js to have the relevant link showing and either show or clear the list of recipes based on the link clicked.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,5 @@
 $(function () {
+  $(".hide_user_recipes").hide();
   $(".get_user_recipes").on("click", function(e) {
     e.preventDefault();
 
@@ -9,5 +10,14 @@ $(function () {
     });
   $(".get_user_recipes").hide();
   $(".hide_user_recipes").show();
+  });
+
+
+  $(".hide_user_recipes").on("click", function(e) {
+    e.preventDefault();
+
+    $("div#recipes ol#recipe_list").empty();
+    $(".get_user_recipes").show();
+    $(".hide_user_recipes").hide();
   });
 });
