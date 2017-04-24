abb3b09b837695f26be74fe7284ecc621213d722
Nathan <admin@bigrocketgames.com>
2017-04-23 20:04:02 -0700

    Updated the AJAX get request to alert the user if the get request failed.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,11 +3,15 @@ $(function () {
   $(".get_user_recipes").on("click", function(e) {
     e.preventDefault();
 
-    $.get(this.href, function(json) {
+    $.get(this.href, function() {
+    }).done(function(json) {
       json.forEach(function(recipe) {
         $("div#recipes ol#recipe_list").append("<li>" + "<a href=/user/" + recipe.user.id + "/recipes/" + recipe.id + ">" + recipe.name + "</a></li>");
       }, this);
+    }).fail(function() {
+      alert("Something didn't work!!");
     });
+    
   $(".get_user_recipes").hide();
   $(".hide_user_recipes").show();
   });
