02d1589a2b6a938e003ca37c2d4eff086df951ae
Nathan <admin@bigrocketgames.com>
2017-05-06 21:17:26 -0700

    recipe_serializer.rb - added reviews layer to the serializer to include those when json is requested.
    recipe#show page - added code to change reviews for the reuested recipe via AJAX.  Added comments to clarify some code.

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -2,4 +2,8 @@ class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :instructions
   belongs_to :user
   has_many :quantities
+  has_many :reviews
+  class ReviewSerializer < ActiveModel::Serializer
+    attributes :title, :content, :updated_at
+  end
 end
